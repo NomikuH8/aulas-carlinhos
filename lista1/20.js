662084e760c43620370190547b46917d500dc187
NomikuH8 <79486063+NomikuH8@users.noreply.github.com>
2022-04-13 12:42:05 +0000

    terminando 20, falta transpor matriz

--- lista1/20.js
+++ lista1/20.js
@@ -1,10 +1,21 @@
 document.write('<h1>Transposição de matriz 5x5</h1>')
 let wrapper = document.createElement('div')
+
+wrapper.id = 'matriz'
 document.querySelector('body').appendChild(wrapper)
+
+let linha1 = []
 
 for (let i = 0; i < 26; i++) {
     let elmt = document.createElement('input')
     elmt.type = 'number'
     elmt.id = 'campo' + i
     wrapper.appendChild(elmt)
+    if (elmt.id == 'campo' + )
+}
+
+document.write('<button onclick="transpor()">Calcular</button>')
+
+function transpor() {
+
 }
