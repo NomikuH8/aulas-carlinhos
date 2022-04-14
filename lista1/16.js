4722e17e1f9f48ef25ad83ac9e4cbb9a456542cc
NomikuH8 <79486063+NomikuH8@users.noreply.github.com>
2022-04-14 13:48:59 +0000

    forgor to rrite

--- lista1/16.js
+++ lista1/16.js
@@ -5,3 +5,5 @@ for (let i = 0; i < 11; i++)
     nums[i] = prompt('insira o numero de sua escolha. (restam: ' + (10-i) + ')')
 
 sortedNums = nums.sort((a, b) => {return b - a})
+
+document.write(sortedNums)
