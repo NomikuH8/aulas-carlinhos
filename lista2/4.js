a00ce6e5e3003b8659cf4fda6dfbdf423f038c0c
NomikuH8 <sofaco.checkin@gmail.com>
2022-04-14 21:25:20 -0300

    finalizado lista2/4

---
+++ lista2/4.js
@@ -0,0 +1,35 @@
+function main() {
+    let cont = 0
+    let numbers = []
+    let divisible = []
+
+    while (cont < 15) {
+        // ler a resposta do usuário
+        let input = prompt('Insira um número: (falta mais ' + (14 - cont) + ' números)')
+        // checar se a resposta do usuário possui algum
+        // caracter que não seja número.
+        // se input for "", ele retornará 0, então não cai
+        // na regex.
+        if (input.match(/\D+/gm) !== null) {
+            alert('Você precisa inserir um número, sem caracteres especiais ou letras!')
+            // vai para a proxima iteracao
+            continue
+        }
+
+        numbers.push(Number(input))
+        cont++
+    }
+
+    for (let i of numbers) {
+        // excluindo 0's por que não faz muito sentido,
+        // visto que todo número é divisível por 0.
+        if (i != 0 && i % 7 === 0)
+            divisible.push(i)
+    }
+
+    document.querySelector('#numbers').innerHTML = numbers
+    document.querySelector('#divisible').innerHTML = divisible
+    document.querySelector('#divisible').innerHTML += `. Totalizando ${divisible.length} número(s)`
+}
+
+main()
